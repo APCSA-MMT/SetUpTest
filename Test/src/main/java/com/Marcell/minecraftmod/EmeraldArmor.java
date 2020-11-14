62210fc89b2b8c2e41015fb4c77bd824a2313baf
Marcell-P <marcell.placencia@gmail.com>
2020-11-13 19:14:13 -0500

    Completed Emerald Armor Base
    Completed the base of the mod.

---
+++ Test/src/main/java/com/Marcell/minecraftmod/EmeraldArmor.java
@@ -0,0 +1,37 @@
+package com.Marcell.minecraftmod;
+
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+@Mod("emerald")
+public class EmeraldArmor {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public EmeraldArmor() {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+
+    }
+
+}
