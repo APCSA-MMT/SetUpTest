a1d546dd5f3cc945c77fa45ad415879926beccd9
Ruppity <lorenzoeros03@gmail.com>
2020-11-15 01:43:20 -0500

    Got ready to code
    Dumb long process overwith

--- src/main/java/com/apscammt/emeraldarmour/ExampleMod.java
+++ src/main/java/com/apscammt/emeraldarmour/ExampleMod.java
@@ -1,6 +1,6 @@
-package com.Marcell.minecraftmod;
+package com.apscammt.emeraldarmour;
 
-
+import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -9,29 +9,23 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
-@Mod("emerald")
-public class EmeraldArmor {
-
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("emeraldarmour")
+public class ExampleMod
+{
     private static final Logger LOGGER = LogManager.getLogger();
-
-    public EmeraldArmor() {
-
+    public ExampleMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-    private void setup(final FMLCommonSetupEvent event) {
-
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
-
     private void doClientStuff(final FMLClientSetupEvent event) {
-
-
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
-
 }
